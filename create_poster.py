7fa219ca44d32bd9d7a8966eac31427450947a73
Jude Foos <jfoos14@simons-rock.com>
2015-10-06 10:29:49 -0400

    adding posters for this week

--- create_poster.py
+++ create_poster.py
@@ -3,8 +3,10 @@ import argparse
 p = argparse.ArgumentParser()
 p.add_argument('-o', type=argparse.FileType('w'), metavar='destfile',
                help='File to be written to.')
-p.add_argument('--image', type=str, help='Background image for your poster.')
+p.add_argument('--image', '-i', type=str, help='Background image for your poster.')
 flags = p.parse_args()
+
+image = r"" + input("Where is the image you want to use?")
 
 top = r"""\documentclass{beamer}
 
